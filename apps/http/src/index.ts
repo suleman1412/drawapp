b83796e8486cb6adec08450c2c2ec2a8c06aa318
Suleman <sulemankarigar1412@gmail.com>
2025-02-20 21:50:02 +0530

    authrouter

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -1,89 +1,19 @@
-import express from 'express'
+import express, { Router } from 'express'
 import { JWT_SECRET } from '@repo/backend-common/config'
 import { AuthSchema, RoomSchema } from "@repo/common/schema"
 import jwt from 'jsonwebtoken'
 import { authMiddleware } from './middleware'
 import { prismaClient } from '@repo/db/db'
 import bcrypt from 'bcrypt'
+import { authRouter } from './routes/authRouter';
 
 const app = express()
+
+
 app.use(express.json())
+app.use('/auth', authRouter)
 
-app.post('/signup', async (req, res) => {
-    const { data, success, error } = AuthSchema.safeParse(req.body)
-    if(!success && !data){
-        res.json({
-            error: error
-        })
-        return;
-    }
-    try{
-        const hashedPassword = await bcrypt.hash(data.password, 5)
-        const user = await prismaClient.user.create({
-            data: {
-                username: data.username,
-                password: hashedPassword
-            }
-        })
 
-        res.status(201).json({
-            message: `Signed up ${data.username}, User added to DB !`,
-            userId: user.id
-        })
-    } catch(err){
-        console.error("Error creating user:", err);
-        res.status(500).json({
-            message: "Failed to create user",
-            error: err
-        });
-    }
-})
-app.post('/signin', async (req, res) => {
-    const { data, success, error } = AuthSchema.safeParse(req.body)
-    if(!success){
-        res.json({
-            error: error
-        })
-        return;
-    }
-    try{
-        const user = await prismaClient.user.findUnique({
-            where: {
-                username: data.username
-            }
-        })
-
-        if(!user){
-            res.status(401).json({
-                message: "User doesn't exist"
-            })
-            return;
-        }
-
-        const match = await bcrypt.compare(data.password, user.password)
-        if(!match){
-            res.status(401).json({
-                message: "Incorrect password"
-            })
-            return;
-        }
-
-        const token = jwt.sign({
-            userId: user.id
-        }, JWT_SECRET)
-
-        res.json({
-            message: `Signed in ${data.username} !`,
-            token: token
-        })
-    } catch(err){
-        console.error("Error signing up:", err);
-        res.status(500).json({
-            message: "Failed to sign in",
-            error: err
-        });
-    }
-})
 app.post('/createRoom', authMiddleware, async(req, res) => {
     const { data, success, error } = RoomSchema.safeParse({
         ...req.body,
@@ -111,6 +41,7 @@ app.post('/createRoom', authMiddleware, async(req, res) => {
         }
     })
 })
+
 
 
 app.listen(3001, () => console.log('server on port 3001'))
---
+++ apps/http/src/routes/authRouter.ts
@@ -0,0 +1,84 @@
+import { AuthSchema } from "@repo/common/schema";
+import { Router } from "express";
+import bcrypt from 'bcrypt'
+import { prismaClient } from "@repo/db/db";
+import jwt from 'jsonwebtoken'
+import { JWT_SECRET } from "@repo/backend-common/config";
+
+export const authRouter: Router = Router()
+
+authRouter.post('/signup', async (req, res) => {
+    const { data, success, error } = AuthSchema.safeParse(req.body)
+    if(!success && !data){
+        res.json({
+            error: error
+        })
+        return;
+    }
+    try{
+        const hashedPassword = await bcrypt.hash(data.password, 5)
+        const user = await prismaClient.user.create({
+            data: {
+                username: data.username,
+                password: hashedPassword
+            }
+        })
+
+        res.status(201).json({
+            message: `Signed up ${data.username}, User added to DB !`,
+            userId: user.id
+        })
+    } catch(err){
+        console.error("Error creating user:", err);
+        res.status(500).json({
+            message: "Failed to create user",
+            error: err
+        });
+    }
+})
+authRouter.post('/signin', async (req, res) => {
+    const { data, success, error } = AuthSchema.safeParse(req.body)
+    if(!success){
+        res.json({
+            error: error
+        })
+        return;
+    }
+    try{
+        const user = await prismaClient.user.findUnique({
+            where: {
+                username: data.username
+            }
+        })
+
+        if(!user){
+            res.status(401).json({
+                message: "User doesn't exist"
+            })
+            return;
+        }
+
+        const match = await bcrypt.compare(data.password, user.password)
+        if(!match){
+            res.status(401).json({
+                message: "Incorrect password"
+            })
+            return;
+        }
+
+        const token = jwt.sign({
+            userId: user.id
+        }, JWT_SECRET)
+
+        res.json({
+            message: `Signed in ${data.username} !`,
+            token: token
+        })
+    } catch(err){
+        console.error("Error signing up:", err);
+        res.status(500).json({
+            message: "Failed to sign in",
+            error: err
+        });
+    }
+})
