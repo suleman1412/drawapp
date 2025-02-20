01e8ac1458eb6556b6fff908c67ed6b434064905
Suleman <sulemankarigar1412@gmail.com>
2025-02-21 00:04:03 +0530

    wsinit

--- apps/ws/src/index.ts
+++ apps/ws/src/index.ts
@@ -1,17 +1,40 @@
 import { WebSocketServer } from 'ws';
+import { JWT_SECRET } from '@repo/backend-common/config'
+import jwt from 'jsonwebtoken'
 
 const wss = new WebSocketServer({ port: 8080 })
 
-wss.on('connection', (socket) => {
-    
+wss.on('connection', (socket, req) => {
     // on error close the port
     socket.on('error', (err) => {
         console.error(err)
         socket.close()
     })
 
+    const url = req.url;
+    if(!url){
+        console.log('no url')
+        socket.close()
+        return;
+    }
+    
+    const queryParams = new URLSearchParams(url.split('?')[1]);
+
+    const token = queryParams.get('token')
+    if(!token){
+        console.log('token not provided')
+        socket.close()
+        return;
+    }
+
+    const decoded = jwt.verify(token, JWT_SECRET)
+    if(!decoded){
+        console.log("Invalid token")
+        socket.close()
+        return;
+    }
     // On any message, send back 'pong'
-    socket.on('message', () => {
-        socket.send('pong')
+    socket.on('message', (message) => {
+        socket.send(message.toString())
     })
 })
