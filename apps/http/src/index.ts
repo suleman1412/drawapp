c272e3ffae56b0b6c5305cecd9d7efaae8f04504
Suleman <sulemankarigar1412@gmail.com>
2025-02-20 18:35:16 +0530

    base layer

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -1,19 +1,62 @@
 import express from 'express'
+import { JWT_SECRET } from '@repo/backend-common/config'
+import { AuthSchema, RoomSchema } from "@repo/common/schema"
+import jwt from 'jsonwebtoken'
+import { authMiddleware } from './middleware'
 
 const app = express()
 app.use(express.json())
 
 app.post('/signup', (req, res) => {
-    const body = req.body;
-    console.log(body)
+    const { data, success, error } = AuthSchema.safeParse(req.body)
+    if(!success){
+        res.json({
+            error: error
+        })
+        return;
+    }
+    res.json({
+        message: `Signed up ${data.username} !`
+    })
 })
 app.post('/signin', (req, res) => {
-    const body = req.body;
-    console.log(body)
+    const { data, success, error } = AuthSchema.safeParse(req.body)
+    if(!success){
+        res.json({
+            error: error
+        })
+        return;
+    }
+    // const userId = fetch from db
+    const userId = "1"
+    const token = jwt.sign({
+        userId: userId
+    } , JWT_SECRET)
+    
+    req.headers['authorization'] = token
+    
+    res.json({
+        message: `Signed in ${data.username} !`,
+        token: token
+    })
 })
-app.post('/createRoom', (req, res) => {
-    const body = req.body;
-    console.log(body)
+app.post('/createRoom', authMiddleware, (req, res) => {
+    const { data, success, error } = RoomSchema.safeParse({
+        ...req.body,
+        // @ts-ignore
+        userId: req.userId
+    })
+    if (!success){
+        res.json({
+            error: error
+        })
+        return;
+    }
+
+    res.status(200).json({
+        // @ts-ignore
+        message: `Room created for ${req.userId} : ${req.body}` 
+    })
 })
 
 app.listen(3001, () => console.log('server on port 3001'))
---
+++ packages/common/src/schema.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod'
+
+export const AuthSchema = z.object({
+    username: z
+        .string()
+        .min(3, {message: "Username has to be minimum of 3 letters"})
+        .max(10, {message: "Username has to be maximum of 10 letters"}),
+    password: z
+        .string()
+        .min(5, {message: "Password has to be minimum of 5 letters"})
+        .max(12, {message: "Password has to be maximum of 20 letters"})
+})
+
+export const RoomSchema = z.object({
+    roomId: z.number().nonnegative(),
+    userId: z.number().nonnegative()
+})
